d1a36c393484b3d2b1222b2e793340f62db1e481
ChairHomard45 <delepaut.clement@gmail.com>
2025-07-01 20:52:19 +0200

    Classes

---
+++ Frontend/HeaderSimulationPanel.cs
@@ -0,0 +1,28 @@
+﻿using Frontend.Observer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Frontend
+{
+    class HeaderSimulationPanel : Panel, ISimulationPanel
+    {
+        public void OnCompleted()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnError(Exception error)
+        {
+            Console.WriteLine("Erreur : ", error.Message);
+        }
+
+
+        public void OnNext(SimulationNotification value)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Frontend/Panels/PanelComponent.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Frontend.Panels
+{
+    public abstract class PanelComponent : Panel
+    {
+
+
+        public void OnCompleted()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnError(Exception error)
+        {
+            Console.WriteLine("Erreur : ", error.Message);
+        }
+
+        public void OnNext(SimulationNotification value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Show() => this.Visible = true;
+        public void Hide() => this.Visible = false;
+    }
+}
